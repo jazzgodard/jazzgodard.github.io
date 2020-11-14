4e35dde72f6f3748034806ea9a7a44264fb7d431
Bob Baxter <bob.e.baxter@gmail.com>
2020-11-13 18:20:29 -0600

    added portfolio modal

--- src/App/App.js
+++ src/App/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 
 import Portfolio from '../Portfolio/Portfolio';
 import MyNavbar from '../MyNavbar/MyNavbar';
+import portfolioProjects from '../Portfolio/Portfolio.json';
 
 import '../styles/scss/agency.scss';
 
+// TODO: make theme responsive
 function App() {
   return (
-    <div className="App">
+    <div id="App" className="App">
+      <div id="modal"></div>
       <MyNavbar />
 
       <header className="masthead" id="masthead">
@@ -26,25 +29,25 @@ function App() {
           <div className="row">
             <div className="col-lg-12 text-center">
               <h2 className="section-heading text-uppercase">Services</h2>
-              <h3 className="section-subheading text-muted">I provide a diverse range of digital marketing, internal communication, and event marketing services to meet client needs and provide results-driven success. How do I accomplish this? <br/><br/>Through innovative and effective solutions.</h3>
+              <h3 className="section-subheading text-muted">I provide a diverse range of digital marketing, internal communication, and event marketing services to meet client needs and provide results-driven success through innovative and effective solutions.</h3>
             </div>
           </div>
-          <div className="row text-center justify-content-center">
-            <div className="col-md-4">
+          <div className="row text-center justify-content-between">
+            <div className="col-md-5">
               <span className="fa-stack fa-4x">
                 <i className="fas fa-circle fa-stack-2x text-primary"></i>
                 <i className="fas fa-copy fa-stack-1x fa-inverse"></i>
               </span>
               <h4 className="service-heading">Content + Copy</h4>
-              <p className="text-muted">Press releases, internal comms, blogs, web copy, and more. Better engagement, succinct writing, and stronger leads. Content creation includes but is not limited to department workflows, client presentations, social media graphics, and digital ads.</p>
+              <p className="text-justify text-muted">Press releases, internal comms, blogs, web copy, and more. Better engagement, succinct writing, and stronger leads. Content creation includes but is not limited to department workflows, client presentations, social media graphics, and digital ads.</p>
             </div>
-            <div className="col-md-4">
+            <div className="col-md-5">
               <span className="fa-stack fa-4x">
                 <i className="fas fa-circle fa-stack-2x text-primary"></i>
                 <i className="fas fa-laptop fa-stack-1x fa-inverse"></i>
               </span>
               <h4 className="service-heading">Social Media Management</h4>
-              <p className="text-muted">Putting social media best practices into place and the best foot forward. Providing both editorial and content calendars to manage and grow a brand or business' following, reach, and leads.</p>
+              <p className="text-justify text-muted">Putting social media best practices into place and the best foot forward. Providing both editorial and content calendars to manage and grow a brand or business' following, reach, and leads.</p>
             </div>
           </div>
         </div>
@@ -58,7 +61,12 @@ function App() {
             </div>
           </div>
           <div className="row">
-            <Portfolio />
+            <div className="Portfolio row">
+              {portfolioProjects.map((project) => <Portfolio
+                  key={project.client}
+                  project={project}
+                />)}
+            </div>
           </div>
         </div>
       </section>
@@ -72,12 +80,12 @@ function App() {
             <div className="col-md-4">
               <ul className="list-inline social-buttons">
                 <li className="list-inline-item">
-                  <a href="/">
+                  <a href="mailto:jazzgodard@gmail.com">
                     <i className="far fa-envelope"></i>
                   </a>
                 </li>
                 <li className="list-inline-item">
-                  <a href="/">
+                  <a href="https://www.linkedin.com/in/jazz-godard-49809494/">
                     <i className="fab fa-linkedin-in"></i>
                   </a>
                 </li>
---
+++ src/Modal/PortfolioModal.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  Button, Modal, ModalHeader, ModalBody,
+} from 'reactstrap';
+import getImage from '../helpers/getImage';
+
+import './PortfolioModal.scss';
+
+export default function PortfolioModal({
+  isOpen, toggle, project, ...props
+}) {
+  const {
+    client,
+    workType,
+    image,
+    url,
+    description,
+    links,
+  } = project;
+
+  const buildLinkButtons = links.map((link) => {
+    const handleBtn = () => {
+      window.open(link.url);
+    };
+    return <Button className="btn btn-primary w-50 m-1" onClick={handleBtn}>{link.mediaType}</Button>;
+  });
+
+  // TODO: make fix images so that they are square and fit a max width
+  return (
+    <Modal isOpen={isOpen} toggle={toggle} className="PorfolioModal">
+      <ModalHeader className="border-0" toggle={toggle}></ModalHeader>
+      <ModalBody className="d-flex flex-row align-items-center">
+        <a className="text-center col-6" href={url} target="_blank" rel="noopener noreferrer">
+          <img alt={`${client} logo`} src={getImage(image)} />
+        </a>
+        <div className="d-flex flex-column col-6">
+          <h3 className="border-primary m-1">{client}</h3>
+          <h4 className="text-secondary m-1">{workType}</h4>
+          <p className="m-1">{description}</p>
+          <div className="d-flex flex-row row-wrap justify-content-left">
+            { buildLinkButtons }
+          </div>
+        </div>
+      </ModalBody>
+    </Modal>
+  );
+}
