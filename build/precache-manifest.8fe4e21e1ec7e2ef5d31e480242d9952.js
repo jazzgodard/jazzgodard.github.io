b7ea2f88bcc219b50a3c2804abe8d6f78492b747
Bob Baxter <bob.e.baxter@gmail.com>
2020-11-23 09:07:14 -0600

    configure-firebase-hosting

---
+++ build/precache-manifest.8fe4e21e1ec7e2ef5d31e480242d9952.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad9c32621b7b0ce7f083adbc2b42d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d93f9121b41cab870029",
+    "url": "/static/css/2.df8eb4f9.chunk.css"
+  },
+  {
+    "revision": "834d1ad88b94351203dc",
+    "url": "/static/css/main.63423c0d.chunk.css"
+  },
+  {
+    "revision": "d93f9121b41cab870029",
+    "url": "/static/js/2.5a66777f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5a66777f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "834d1ad88b94351203dc",
+    "url": "/static/js/main.5aa0cb9a.chunk.js"
+  },
+  {
+    "revision": "37f812a515b4a9624ffc",
+    "url": "/static/js/runtime-main.40368bdf.js"
+  },
+  {
+    "revision": "f8648487f068e002a0ad46321d555add",
+    "url": "/static/media/01-full.f8648487.jpg"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "8fdb36b51df820ac2e7bcf6533327f6f",
+    "url": "/static/media/02-full.8fdb36b5.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "3b263f0ffc3870f7fc6cefee9a86d50b",
+    "url": "/static/media/03-full.3b263f0f.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "c58223e188b4fad806838b3355441f74",
+    "url": "/static/media/04-full.c58223e1.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "408a2be2fc16ddf874a8daac0f81c828",
+    "url": "/static/media/05-full.408a2be2.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "6379a8ad27dee3e9166a9f81b32db785",
+    "url": "/static/media/06-full.6379a8ad.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/static/media/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "e6bcd11c352b789261e94d6e7781dca6",
+    "url": "/static/media/lubo-minar-B-gb_8fgmz8-unsplash.e6bcd11c.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "735fcc25d3d0e12a67a31584927c719e",
+    "url": "/static/media/masthead-pic.735fcc25.jpg"
+  }
+]);
